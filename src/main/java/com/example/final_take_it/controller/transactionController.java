a241e9cbf18794832cb144130f0fbb950a75194e
sky-coding-12 <akashlathiya121212@gmail.com>
2023-04-11 11:26:57 +0530

    get one transaction by id

--- src/main/java/com/example/final_take_it/controller/transactionController.java
+++ src/main/java/com/example/final_take_it/controller/transactionController.java
@@ -21,10 +21,16 @@ public class transactionController {
         return transactionRepo.findAll();
     }
 
+    @GetMapping("getTransaction")
+    public List<Transaction> getTransaction(@RequestParam Long id){
+        List<Transaction> transactions = getAllTransaction();
+        return transactions.stream().filter(u -> Objects.equals(u.getTransactionId(), id)).collect(Collectors.toList());
+    }
+
     @GetMapping("getUserCreditDebit")
     public List<Long> getUserCreditDebit(@RequestParam Long userPhone) {
         List<Transaction> transactions = getAllTransaction();
-        List<Transaction> transactions1 = transactions.stream().filter(t -> Objects.equals(userPhone, t.getUserId())).collect(Collectors.toList());
+        List<Transaction> transactions1 = transactions.stream().filter(t -> Objects.equals(userPhone, t.getUserId())).toList();
         long totalCredit = 0, totalDebit = 0;
         for (Transaction transaction : transactions1) if (Objects.equals(transaction.getCreditDebitStatus(), "credit")) totalCredit += transaction.getAmount();
         for (Transaction transaction : transactions1) if (Objects.equals(transaction.getCreditDebitStatus(), "debit")) totalDebit += transaction.getAmount();
