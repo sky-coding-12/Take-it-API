ef08c905f5c4c5dfa471527b29df954346a8716b
sky-coding-12 <akashlathiya121212@gmail.com>
2023-04-11 12:07:16 +0530

    get time request

--- src/main/java/com/example/final_take_it/controller/timeRequestController.java
+++ src/main/java/com/example/final_take_it/controller/timeRequestController.java
@@ -1,11 +1,14 @@
 package com.example.final_take_it.controller;
 
 import com.example.final_take_it.model.TimeRequest;
+import com.example.final_take_it.model.Transaction;
 import com.example.final_take_it.repositiory.timeRequestRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("timeRequest")
@@ -16,6 +19,13 @@ public class timeRequestController {
     @GetMapping("getAllTimeRequest")
     public List<TimeRequest> getAllTimeRequest(){
         return timeRequestRepo.findAll();
+    }
+
+    @GetMapping("getAllTimeRequestByUser")
+    public List<TimeRequest> getAllTimeRequestByUser(@RequestParam Long userPhone, Long vendorPhone){
+        List<TimeRequest> transactions = getAllTimeRequest();
+        List<TimeRequest> filterByUserId = transactions.stream().filter(u -> Objects.equals(u.getUserId(), userPhone)).collect(Collectors.toList());
+        return filterByUserId.stream().filter(u -> Objects.equals(u.getVendorId(), vendorPhone)).collect(Collectors.toList());
     }
 
     @PostMapping("saveTimeRequest")
