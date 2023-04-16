35b06d0cc1457eb31b4b2839ab3079b0208d4c2b
sky-coding-12 <akashlathiya121212@gmail.com>
2023-04-16 21:25:35 +0530

    add offer loan

--- src/main/java/com/example/final_take_it/controller/loanOfferController.java
+++ src/main/java/com/example/final_take_it/controller/loanOfferController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("loanRequest")
+@RequestMapping("loanOffer")
 public class loanOfferController {
     @Autowired
     private loanOfferRepo loanOfferRepo;
