43b12b5a444eb832e031f412eb52beb2692c01be
sky-coding-12 <akashlathiya121212@gmail.com>
2023-04-16 21:52:57 +0530

    getLoanOfferByVendor

--- src/main/java/com/example/final_take_it/controller/loanOfferController.java
+++ src/main/java/com/example/final_take_it/controller/loanOfferController.java
@@ -3,9 +3,12 @@ package com.example.final_take_it.controller;
 import com.example.final_take_it.model.LoanOffer;
 import com.example.final_take_it.repositiory.loanOfferRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("loanOffer")
@@ -15,11 +18,19 @@ public class loanOfferController {
 
     @GetMapping("getAllNoticeOffer")
     public List<LoanOffer> getAllNoticeOffer(){
-        return loanOfferRepo.findAll();
+        return loanOfferRepo.findAll(Sort.by("loanOfferId").descending());
     }
 
     @PostMapping("saveLoanOffer")
     public LoanOffer saveLoanOffer(@RequestBody LoanOffer loanOffer){
         return loanOfferRepo.save(loanOffer);
+    }
+
+    @GetMapping("getLoanOfferByVendor")
+    public List<LoanOffer> getLoanOfferByVendor(@RequestParam Long bankPhone, Long vendorPhone){
+        List<LoanOffer> loanOffers = getAllNoticeOffer();
+        List<LoanOffer> loanByBank = loanOffers.stream().filter(u -> Objects.equals(u.getBankId(), bankPhone)).collect(Collectors.toList());
+        List<LoanOffer> loanByVendor = loanByBank.stream().filter(u -> Objects.equals(u.getVendorId(), vendorPhone)).collect(Collectors.toList());
+        return loanByVendor;
     }
 }
